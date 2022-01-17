11572437f29dd41fcaf92cd1295088873bd9e6e2
root <nagaraja.hm.0001@gmail.com>
2022-01-17 11:48:15 +0100

    Update: submodule Lin

--- LATEST/Lin.cpp
+++ LATEST/Lin.cpp
@@ -5,3 +5,27 @@ class_Lin Lin;
 FUNC(void, LIN_CODE) class_Lin::InitFunction(void){
 }
 
+FUNC(void, LIN_CODE) class_Lin::CheckWakeup(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::GetVersionInfo(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::SendFrame(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::GoToSleep(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::GoToSleepInternal(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::Wakeup(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::WakeupInternal(void){
+}
+
+FUNC(void, LIN_CODE) class_Lin::GetStatus(void){
+}
+
--- LATEST/Lin.h
+++ LATEST/Lin.h
@@ -5,7 +5,15 @@
 
 class class_Lin{
    public:
-      FUNC(void, LIN_CODE) InitFunction(void);
+      FUNC(void, LIN_CODE) InitFunction      (void);
+      FUNC(void, LIN_CODE) CheckWakeup       (void);
+      FUNC(void, LIN_CODE) GetVersionInfo    (void);
+      FUNC(void, LIN_CODE) SendFrame         (void);
+      FUNC(void, LIN_CODE) GoToSleep         (void);
+      FUNC(void, LIN_CODE) GoToSleepInternal (void);
+      FUNC(void, LIN_CODE) Wakeup            (void);
+      FUNC(void, LIN_CODE) WakeupInternal    (void);
+      FUNC(void, LIN_CODE) GetStatus         (void);
 };
 
 extern class_Lin Lin;
