63ebe6d66405e5bc780e2c959fe398a3df352148
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:26 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Lin

--- LATEST/Lin.cpp
+++ LATEST/Lin.cpp
@@ -113,7 +113,7 @@ FUNC(void, LIN_CODE) module_Lin::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  LIN_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, LIN_CODE) module_Lin::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  LIN_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, LIN_CODE) module_Lin::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  LIN_E_UNINIT
       );
 #endif
    }
