a673a58fc8577736c50fd8ade388ccc55809586a
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:36 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Lin

--- LATEST/Lin.cpp
+++ LATEST/Lin.cpp
@@ -35,6 +35,13 @@ class module_Lin:
       public abstract_module
    ,  public class_Lin_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Lin(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
